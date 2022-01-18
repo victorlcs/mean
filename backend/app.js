b09650ad47929ce7af4a21ebc1aa175f14a3e66e
victorlcs <victor.loh95@gmail.com>
2022-01-18 21:21:13 +0800

    added /api/posts restful call

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,15 @@ app.use((req,res,next)=> {
     next();
 });
 
-app.use((req,res,next)=> {
-    res.send('Hello from express!');
+app.use("/api/posts",(req,res,next)=> {
+    const posts = [
+        {id:'abc123',title:"First server-side post", content:"This is coming from server"},
+        {id:'abc124',title:"First server-side post2", content:"This is coming from server2"}
+    ] 
+    res.status(200).json({
+        message:'post successfully',
+        posts:posts
+    });
 });
 
 module.exports = app;
